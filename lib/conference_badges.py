eb26d376c22a4df9521fe04699942a286ee3e4f1
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-01-20 00:39:43 +0300

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name) 
 
 def batch_badge_creator(names):
-    return None
+    badges = ["Hello, my name is {}.".format(name) for name in names]
+    return badges
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = ["Hello, {}! You'll be assigned to room {}!".format(speaker, room) for room, speaker in enumerate(speakers, start=1)]
+    return room_assignments
+
+    
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for assignment in room_assignments:
+        print(assignment)
